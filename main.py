a87fb17d997b15bfbd2ffb2de6d96b7c16a44d35
shree <singhishree@gmail.com>
2025-07-29 01:53:31 +0700

    works

---
+++ main.py
@@ -0,0 +1,54 @@
+import gpytoolbox as gpy
+import polyscope as ps
+import numpy as np
+from scipy.sparse import csr_matrix, eye
+from scipy.sparse.linalg import inv
+import polyscope.imgui as psim
+
+V, F = gpy.read_mesh('data/spot_low_resolution.obj')
+
+L = gpy.cotangent_laplacian(V, F)
+M = gpy.massmatrix(V,F)
+DT = 1e-3
+N_VERTICES = V.shape[0]
+
+N_STEPS = 100
+
+u = np.zeros(N_VERTICES)
+u[[0, 300, 700]] = 1
+
+
+M_inverse = csr_matrix((1/M.data, M.indices, M.indptr))
+
+A = M_inverse @ L
+matrix = inv(eye(N_VERTICES) + DT * A)
+us = [u.copy()]
+for i in range(N_STEPS-1):
+    u = matrix @ u
+    us.append(u.copy())
+
+# Compute the eigenvalues and eigenvectors
+
+ps.init()
+
+ps_cow = ps.register_surface_mesh("cow", V, F)
+
+# Add the final heat diffusion solution as a scalar quantity
+heat_quantity = ps_cow.add_scalar_quantity("heat diffusion", us[0], enabled=True, vminmax=(0, 1e-2))
+
+# Track the current time step
+current_step = N_STEPS - 1
+
+def callback():
+    global current_step
+    
+    changed, current_step = psim.SliderInt("Time step", current_step, 0, N_STEPS-1)
+    
+    if changed:
+        ps_cow.add_scalar_quantity("heat diffusion", us[current_step], enabled=True, vminmax=(0, 1e-2))
+
+
+ps.set_user_callback(callback)
+
+ps.show()
+
